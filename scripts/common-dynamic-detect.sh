d2aca9a152523485d347d2613fc0f026ce7f7651
Akshath-Nagulapally <140837101+Akshath-Nagulapally@users.noreply.github.com>
2024-06-05 15:12:46 +0530

    Update common-dynamic-detect.sh

--- scripts/common-dynamic-detect.sh
+++ scripts/common-dynamic-detect.sh
@@ -4,14 +4,14 @@
 
 set -euxo pipefail
 
-# Kuernetes Variable Declaration
+# Kubernetes Variable Declaration
 KUBERNETES_VERSION="1.29.0-1.1"
 
 # Disable swap
 sudo swapoff -a
 
 # Keep swap off during reboot
-(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | sudo crontab - || true
 sudo apt-get update -y
 
 # Install CRI-O Runtime
@@ -44,7 +44,7 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cr
 deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
 EOF
 
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+curl -L https://download.opensuse.org/repositories/devel:kubic:/libcontainers:/stable:/cri-o:$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
 
 sudo apt-get update
@@ -76,8 +76,8 @@ sudo apt-get install -y jq
 INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
 
 # Fetch the local IP address
-local_ip=$(ip -4 addr show $INTERFACE | jq -r '.[0].addr_info[] | select(.family == "inet") | .local')
+local_ip=$(ip -4 addr show "$INTERFACE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
-cat > /etc/default/kubelet << EOF
+cat <<EOF | sudo tee /etc/default/kubelet
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
 EOF
